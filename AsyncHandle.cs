55198a6e07576c2b5fe60d66a0a3219f5366ac52
caonimagfw <51797166+caonimagfw@users.noreply.github.com>
2019-11-20 15:37:30 +0800

    ADD FIRST

---
+++ AsyncHandle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Threading;
+
+namespace TaskSequence
+{
+    /// <summary>
+    /// 异步调用
+    /// </summary>
+    public class AsyncHandle
+    {
+        private readonly SerialQueue queue = new SerialQueue();
+        private int watierTempperature = 0;
+
+        public async Task<int> OpenBoil(string s)
+        {
+
+            await queue.Enqueue(() => {
+              
+                BoilWorking(s);
+            });     
+
+            return watierTempperature;
+        }
+
+        private int BoilWorking(string ss)
+        {
+            Console.WriteLine(ss);
+            Thread.Sleep(500);
+            watierTempperature = watierTempperature + 10;
+            string s = "Thread ID:" + Thread.CurrentThread.ManagedThreadId + " Boil temperature is :" + watierTempperature;
+            s = DateTime.Now.ToString("yyyy-MM-hh HH:mm:ss:fff ") + s;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(s);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            
+            return 1;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSequence
+{
+
+    class Program
+    {
+        static AsyncHandle ah = new AsyncHandle();
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 100; i++)
+            {
+
+                Console.BackgroundColor = ConsoleColor.Black;
+                ah.OpenBoil(i.ToString());
+                Console.BackgroundColor = ConsoleColor.Black;
+
+            }
+            Console.WriteLine("Main Call End******");
+            Console.ReadLine();
+        }
+
+
+    }
+}
